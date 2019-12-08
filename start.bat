1cb1960e756794121f29f3bbe2d3de3fbe875133
yann ARMAND <yarmand@yammer-inc.com>
2019-12-07 23:29:20 -0800

    share dockervolumes with windows host

--- start.bat
+++ start.bat
@@ -9,6 +9,20 @@ docker volume create docker-dev-env_src
 
 docker build -t docker-dev-env .
 
+docker run --name samba ^
+        --net=host ^
+        -e USERID=0 ^
+        -e GROUPIP=0 ^
+        -v docker-dev-env_home:/home ^
+        -v docker-dev-env_opt:/opt ^
+        -v docker-dev-env_src:/src ^
+        --rm ^
+        -d ^
+        dperson/samba ^
+          -s "HOME;/home;yes;no" ^
+          -s "OPT;/opt;yes;no" ^
+          -s "SRC;/src;yes;no"
+
 docker run --rm -it ^
         --name docker-dev-env ^
         -v docker-dev-env_home:/root ^
@@ -18,4 +32,4 @@ docker run --rm -it ^
         -v /var/run/docker.sock:/var/run/docker.sock ^
         -v /usr/local/bin/docker:/usr/local/bin/docker ^
         -p 9022:22 ^
-        docker-dev-env 
+        docker-dev-env
